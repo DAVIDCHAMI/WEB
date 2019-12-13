8fd4571a7596e5220260417a101e45c8ef2aba07
Daniel Henao <dhenao@todo1.net>
2019-12-13 11:47:11 -0500

    Creacion de question para productos ocultos

--- SVP/src/main/java/co/com/bancolombia/certificacion/svp/questions/inicio/saldosporproducto/SaldosPorProductoOcultos.java
+++ SVP/src/main/java/co/com/bancolombia/certificacion/svp/questions/inicio/saldosporproducto/SaldosPorProductoOcultos.java
@@ -1,15 +1,18 @@
 package co.com.bancolombia.certificacion.svp.questions.inicio.saldosporproducto;
 
+import co.com.bancolombia.certificacion.svp.models.DatosPrueba;
+import co.com.bancolombia.certificacion.svp.userinterface.inicio.InicioPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+
+import static co.com.bancolombia.certificacion.svp.userinterface.inicio.InicioPage.MSJ_PRODUCTOS_OCULTOS;
+import static co.com.bancolombia.certificacion.svp.utilities.constant.ConstantManager.MENSAJE_RESPUESTA;
 
 public class SaldosPorProductoOcultos implements Question<Boolean> {
 
     @Override
     public Boolean answeredBy(Actor actor) {
-
-        //mensaje
-        return null;
+        return MSJ_PRODUCTOS_OCULTOS.of(DatosPrueba.getMap().get(MENSAJE_RESPUESTA).toString()).resolveFor(actor).isVisible();
     }
 
     public static SaldosPorProductoOcultos visualizaElMensajeDeProductosOcultos(){
--- SVP/src/main/java/co/com/bancolombia/certificacion/svp/userinterface/inicio/InicioPage.java
+++ SVP/src/main/java/co/com/bancolombia/certificacion/svp/userinterface/inicio/InicioPage.java
@@ -5,5 +5,6 @@ import net.serenitybdd.screenplay.targets.Target;
 public class InicioPage {
 
     public static final Target LBL_PRODUCTOS = Target.the("Productos").locatedBy("");
+    public static final Target MSJ_PRODUCTOS_OCULTOS = Target.the("Productos ocultos").locatedBy("");
 
 }
