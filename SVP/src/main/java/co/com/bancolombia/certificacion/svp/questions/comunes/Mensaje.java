2453b791fbe8b361af9cc43234ded1c3c151d0eb
Daniel Henao <dhenao@todo1.net>
2019-12-19 07:36:11 -0500

    ajsutes sonar

--- SVP/src/main/java/co/com/bancolombia/certificacion/svp/questions/comunes/Mensaje.java
+++ SVP/src/main/java/co/com/bancolombia/certificacion/svp/questions/comunes/Mensaje.java
@@ -1,7 +1,6 @@
 package co.com.bancolombia.certificacion.svp.questions.comunes;
 
 import co.com.bancolombia.certificacion.svp.models.DatosPrueba;
-import co.com.bancolombia.certificacion.svp.utilities.constant.ConstantManager;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 
