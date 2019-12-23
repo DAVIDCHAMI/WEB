7c429666c4358abe64667e79432f80ac29acef45
Daniel Henao <dhenao@todo1.net>
2019-12-23 16:58:21 -0500

    ajustes sonar

--- SVP/src/main/java/co/com/bancolombia/certificacion/svp/tasks/recordarusuario/Recordar.java
+++ SVP/src/main/java/co/com/bancolombia/certificacion/svp/tasks/recordarusuario/Recordar.java
@@ -4,7 +4,6 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
 
 import static co.com.bancolombia.certificacion.svp.tasks.recordarusuario.LlenarFormularioRecordarUsuario.llenaElFormularioDelRecordarUsuario;
 import static co.com.bancolombia.certificacion.svp.userinterface.autenticacion.AutenticacionPage.BTN_OLVIDASTE_TU_USUARIO;
