10705be1c89b3b3a497d210e431d8c691db474cc
DAVID_MORENO_M <chamirojo.16@hotmail.es>
2020-02-03 17:08:05 -0500

    Se limplia scripts correspondientes a la HU cierre de sesión

--- SVP/src/main/java/co/com/bancolombia/certificacion/svp/questions/cerrarsesion/ValidacionDeCierreDeSesion.java
+++ SVP/src/main/java/co/com/bancolombia/certificacion/svp/questions/cerrarsesion/ValidacionDeCierreDeSesion.java
@@ -1,6 +1,5 @@
 package co.com.bancolombia.certificacion.svp.questions.cerrarsesion;
 
-import co.com.bancolombia.certificacion.svp.interactions.comunes.CerrarSesionSVP;
 import co.com.bancolombia.certificacion.svp.interactions.comunes.Esperar;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
@@ -8,7 +7,6 @@ import net.serenitybdd.screenplay.questions.Text;
 
 import static co.com.bancolombia.certificacion.svp.interactions.comunes.CerrarSesionSVP.cerrarSesion;
 import static co.com.bancolombia.certificacion.svp.userinterface.inicio.Inicio.LBL_MENSAJE_VALIDACION_CIERRE_SESION;
-import static co.com.bancolombia.certificacion.svp.utilities.constant.ConstantExcelData.MENSAJE_RESPUESTA;
 
 public class ValidacionDeCierreDeSesion implements Question<String> {
 
@@ -21,7 +19,6 @@ public class ValidacionDeCierreDeSesion implements Question<String> {
     public String answeredBy(Actor actor) {
         actor.attemptsTo(cerrarSesion());
         Esperar.unTiempo();
-        System.out.print(Text.of(LBL_MENSAJE_VALIDACION_CIERRE_SESION).viewedBy(actor).asString() + "HOLA");
         return Text.of(LBL_MENSAJE_VALIDACION_CIERRE_SESION).viewedBy(actor).asString();
     }
 }
